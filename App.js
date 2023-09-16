b27ab0844901b47de4466e86e0aad93ef71418f8
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-09-16 22:28:51 +0700

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text> Demo gitđ</Text>
-      <Text>Ngao luonfdsfdsfdsfdsf</Text>
+      <Text>Ngao luonfdsfdsfdsfdsfsujad ádasdasd</Text>
       <StatusBar style="auto" />
     </View>
   );
