94665ade61668338ff901b1ecd2283812a8a7ec7
ThanhLuyen2003 <thanhluyen732003@gmail.com>
2023-09-16 22:25:19 +0700

    Update App.js
    cccc

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text> Demo git</Text>
-      <Text>Ngao luon</Text>
+      <Text>Ngao luon :ĐĐĐĐĐĐĐ</Text>
       <StatusBar style="auto" />
     </View>
   );
