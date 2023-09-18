b023fff1e1b8fc6d009aa7bad2072193df1d5411
ThanhLuyen2003 <thanhluyen732003@gmail.com>
2023-09-18 14:26:11 +0700

    Create DatLich.js

---
+++ Components/DatLich.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
+
+const DatLich = (props) => {
+
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+export default DatLich;
