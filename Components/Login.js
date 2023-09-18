efcc9fc7d35d3db688091bdef5f9d00920279229
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-09-18 19:45:42 +0700

    Update Login.js

--- Components/Login.js
+++ Components/Login.js
@@ -1,21 +1,23 @@
 import React from "react";
-import { View, Image, StyleSheet, Button } from "react-native";
+import { View, Image, StyleSheet, Button, ImageBackground } from "react-native";
 
-const Login = (props) => {
-
+const Login = (props) => { 
     const hi = () => {
-
         props.navigation.navigate('HomeTab');
-
     }
 
     return (
         <View>
+            {/* <Image source={require('../Images/Barbershop.png')}/> */}
+            {/* <Button onPress={hi} title="Hi" /> */}
+            <ImageBackground  blurRadius={2} style={{flex:1,height:690}} source={require('../Images/image.png')}>
 
-            <Button onPress={hi} title="Hi" />
+            </ImageBackground>
 
         </View>
     );
 }
+const style=StyleSheet.create({
 
+})
 export default Login;
