88ad291e64e2a3b1587ec97184ef1c0c63ade36e
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-09-18 20:58:27 +0700

    Update Login.js

--- Components/Login.js
+++ Components/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Image, StyleSheet, Button, ImageBackground } from "react-native";
+import { View, Image, StyleSheet, Button, ImageBackground,Text } from "react-native";
 
 const Login = (props) => { 
     const hi = () => {
@@ -7,16 +7,33 @@ const Login = (props) => {
     }
 
     return (
-        <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
-            {/* <Image source={require('../Images/Barbershop.png')}/> */}
-            {/* <Button onPress={hi} title="Hi" /> */}
-            <ImageBackground  blurRadius={2}  source={require('../Images/bannerLog.png')}>
-            <View style={{flex:1}}>
-            <Image style={{width:300,height:300,borderRadius:50}} source={require('../Images/Barbershop.png')}/>
-            </View>
+        
+
+            <ImageBackground  blurRadius={2} style={{flex:1}}  source={require('../Images/Bannerr.png')}>
+                <View style={{flex:1}}>
+    <View style={{backgroundColor:'rgba(46,134,193,0.2'}}>
+    </View>
+    <View style={{backgroundColor:"#99FF99",borderTopLeftRadius:20,borderTopRightRadius:20}}>
+      <View style={{position:"absolute",justifyContent:'center',width:"100%",alignItems:"center"}}>
+      <Image style={{width:300,height:300,borderRadius:50}} source={require('../Images/Barbershop.png')}/>
+      </View>
+     
+      
+    
+    
+    {/* <TouchableOpacity onPress={logOut}>
+        <View style={{flexDirection:'row',marginLeft:16}}>
+          <Text style={{fontSize:18}}>Logout</Text>
+          <Icon style={{fontSize:18,justifyContent:'center',paddingLeft:300}} name='arrow-right'size={20}/>
+        </View>
+        
+      </TouchableOpacity> */}
+    </View>
+    
+  </View>
             </ImageBackground>
 
-        </View>
+        
     );
 }
 const style=StyleSheet.create({
