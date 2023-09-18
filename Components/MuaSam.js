ba21fa945888e2eb8464f7e3039eb7efeb4a325e
ThanhLuyen2003 <thanhluyen732003@gmail.com>
2023-09-18 14:26:43 +0700

    Create MuaSam.js

---
+++ Components/MuaSam.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
+
+const MuaSam = (props) => {
+
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+export default MuaSam;
