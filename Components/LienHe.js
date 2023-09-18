2e6671417ece5eb6dbeb8bd3b8849ce80bb897a9
ThanhLuyen2003 <thanhluyen732003@gmail.com>
2023-09-18 14:26:37 +0700

    Create LienHe.js

---
+++ Components/LienHe.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
+
+const LienHe = (props) => {
+
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+export default LienHe;
