3748d411dd032086e4e970be86566fbcb2e267a2
ThanhLuyen2003 <thanhluyen732003@gmail.com>
2023-09-18 23:46:37 +0700

    Update MuaSam.js

--- Components/MuaSam.js
+++ Components/MuaSam.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { View, Image, StyleSheet,Text } from "react-native";
+import { View, Image, StyleSheet, Text } from "react-native";
 
 const MuaSam = (props) => {
 
     return (
         <View>
-                <Text>Mua Sam</Text>
+            <Text >Mua Sam</Text>
         </View>
     );
 }
