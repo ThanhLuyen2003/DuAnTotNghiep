40488cf7107fd423b66ef5b38f12503e080db21f
ThanhLuyen2003 <thanhluyen732003@gmail.com>
2023-09-18 14:26:42 +0700

    Create ManChao.js

---
+++ Components/ManChao.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
+
+const ManChao = (props) => {
+
+
+    React.useEffect(() => {
+
+        setTimeout(() => {
+            props.navigation.navigate('Login');
+        }, 2000);
+    });
+
+    return (
+        <View>
+
+            <Image source={require('../Images/home.png')} style={{ width: "100%", height: 300, marginTop: 300 }} />
+
+        </View>
+    )
+
+
+}
+
+export default ManChao;
