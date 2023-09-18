9bf003ffc79a58bd77fc837b57cb3a3cf70a968a
ThanhLuyen2003 <thanhluyen732003@gmail.com>
2023-09-18 14:26:45 +0700

    Create SignUp.js

---
+++ Components/SignUp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
+
+const SignUp = (props) => {
+
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+export default SignUp;
