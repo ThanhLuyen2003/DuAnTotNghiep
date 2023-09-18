532c8f8193a795d7d8d2d3440b1199a61bae9fa6
ThanhLuyen2003 <thanhluyen732003@gmail.com>
2023-09-18 14:26:35 +0700

    Create Home.js

---
+++ Components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
+
+const Home = (props) => {
+
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+export default Home;
