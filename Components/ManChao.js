fd2405131e667f0e52c3356378af5ddeea50d077
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-09-19 19:41:26 +0700

    Update ManChao.js

--- Components/ManChao.js
+++ Components/ManChao.js
@@ -1,22 +1,25 @@
 import React from "react";
-import { View, Image, StyleSheet } from "react-native";
+import { View, Image, StyleSheet,ImageBackground } from "react-native";
+
 
 const ManChao = (props) => {
 
 
-    React.useEffect(() => {
+    // React.useEffect(() => {
 
-        // setTimeout(() => {
-        //     props.navigation.navigate('Login');
-        // }, 2000);
-    });
+    //     setTimeout(() => {
+    //         props.navigation.navigate('Login');
+    //     }, 2000);
+    // });
 
     return (
-        <View>
+        <ImageBackground blurRadius={1} style={{ flex: 1 }} source={require('../Images/nenbarber.jpg')}>
+            <Image  source={require('../Images/Barbershop.png')}  />
+        </ImageBackground>
 
-            <Image source={require('../Images/homee.png')} style={{ width: "100%", height: 300, marginTop: 300 }} />
-
-        </View>
+            
+            
+        
     )
 
 
