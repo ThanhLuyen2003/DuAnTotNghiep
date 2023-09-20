7f58a21770d9a34310fe99b49b5e3b6acf0d6832
ThanhLuyen2003 <thanhluyen732003@gmail.com>
2023-09-20 11:34:32 +0700

    Update DatLich.js

--- Components/DatLich.js
+++ Components/DatLich.js
@@ -1,25 +1,41 @@
 import React from "react";
-import { View, Image, StyleSheet, Text, FlatList } from "react-native";
+import { View, Image, StyleSheet, Text, FlatList, TouchableHighlight, TouchableOpacity } from "react-native";
 import { Calendar } from "react-native-calendars";
 import { useState } from "react";
 import DatePicker from "react-native-date-picker";
+
+const numColumn = 4;
+
 const DatLich = (props) => {
 
     const [chonNgay, setchonNgay] = useState("");
+    const [chonGio, setchonGio] = useState("");
 
     const today = new Date();
 
     const startDay = today.getFullYear() + "-" + 0 + (today.getMonth() + 1) + "-" + today.getDate();
 
     const [khungGio, setkhungGio] = useState([
-        "10:70", "10:00", "10:00", "10:00", "10:00"
+        "09:00", "09:30", "10:00", "10:30", "11:00", "11:30",
+        "12:00", "12:30", "13:00", "13:30", "14:00", "14:30",
+        "15:00", "15:30", "16:00", "16:30", "17:00", "17:30",
+        "18:00", "18:30", "19:00", "19:30", "20:00", "20:30",
+
     ])
 
+    const hi = (item, index) => {
+        selected: true
+    }
+
+
     const itemm = ({ item }) => {
+
+        let { itemm } = stylee;
+
         return (
-            <View style={{ borderColor: '#CD853F', borderWidth: 1, padding: 10, width: 100, margin: 5, alignItems: 'center', borderRadius: 20 }}>
-                <Text> {item}</Text>
-            </View>
+            <TouchableOpacity onPress={hi(item)} style={[stylee.itemm, { backgroundColor: item.selected ? '#CD853F' : 'white' }]}>
+                <Text > {item}</Text>
+            </TouchableOpacity>
         )
     }
 
@@ -33,21 +49,29 @@ const DatLich = (props) => {
             <Calendar
                 style={{ borderRadius: 10, borderColor: 'black', borderWidth: 1, shadowOpacity: 0.2 }}
                 onDayPress={(date) => { setchonNgay(date.dateString) }}
-                hideExtraDays={true}
                 markedDates={{
                     [chonNgay]: { selected: true }
                 }}
                 minDate={startDay}
             />
 
-            <View style={{ height: 40, width: "100%", marginTop: 20 }}>
+            <View style={{ height: 40, width: "100%", }}>
                 <Text style={{ fontSize: 20, fontWeight: 'bold', alignSelf: 'flex-start', marginTop: 5 }}>Chọn  khung giờ</Text>
             </View>
 
-            <FlatList data={khungGio}
-                renderItem={itemm}
+            <View style={{ width: '100%', height: 200 }}>
+                <FlatList
+                    data={khungGio}
+                    renderItem={itemm}
+                    numColumns={3}
+                    style={{ height: 50 }}
+                />
+            </View>
 
-            />
+            <TouchableOpacity onPress={hi} style={{ backgroundColor: '#CD853F', width: '90%', height: 40, borderRadius: 50, alignItems: 'center', alignSelf: 'center' }}  >
+                <Text style={{ color: 'white', fontSize: 20, marginTop: 10, }}>Tiếp tục</Text>
+            </TouchableOpacity>
+
 
 
 
@@ -55,5 +79,20 @@ const DatLich = (props) => {
 
     );
 }
+
+const stylee = StyleSheet.create({
+    itemm: {
+        borderColor: '#CD853F',
+        borderWidth: 1,
+        padding: 10,
+        width: 100,
+        margin: 5,
+        alignItems: 'center',
+        borderRadius: 20,
+        height: 'auto',
+        flex: 1,
+
+    }
+})
 
 export default DatLich;
