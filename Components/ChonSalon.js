375fec70156814d0021e3a4193135dfa3d4637f2
ThanhLuyen2003 <thanhluyen732003@gmail.com>
2023-10-02 22:09:38 +0700

    Create ChonSalon.js

---
+++ Components/ChonSalon.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Image, StyleSheet, Text } from "react-native";
+
+const ChonSalon = (props) => {
+
+    return (
+        <View>
+            <Text>chonNgay</Text>
+        </View>
+    );
+}
+
+export default ChonSalon;
