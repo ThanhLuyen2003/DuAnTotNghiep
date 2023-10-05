6d0d84ff6b1a90719cd13f6d8e7397b9847b2530
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-10-06 00:39:34 +0700

    Create ChamSocDa.js

---
+++ Components/Componentss/ChamSocDa.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ChamSocDa = () => {
+  return (
+    <View>
+      <Text>ChamSocDa</Text>
+    </View>
+  )
+}
+
+export default ChamSocDa
+
+const styles = StyleSheet.create({})
