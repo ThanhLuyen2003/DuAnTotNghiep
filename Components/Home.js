684eeb95d5277706208712fa4c356ad0cfe98c34
ThanhLuyen2003 <thanhluyen732003@gmail.com>
2023-10-05 19:02:31 +0700

    Update Home.js

--- Components/Home.js
+++ Components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Image, StyleSheet, Text, TextInput, SafeAreaView } from "react-native";
+import { View, Image, StyleSheet, Text, TextInput, SafeAreaView, ScrollView } from "react-native";
 import Icons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 
@@ -10,19 +10,28 @@ const Home = (props) => {
     }
     return (
 
-        <SafeAreaView>
+        <View>
             <View style={{ backgroundColor: 'rgba(251, 154, 69, 0.8)', width: "100%", height: 60, flexDirection: "row", alignItems: "center" }}>
                 <View >
                     <Icons style={{ position: "absolute", left: 25, zIndex: 1, top: 7 }} name="magnify" size={25} color="#363636" />
                     <TextInput placeholder="Tìm dịch vụ, cửa hàng, địa điểm,.." style={{ width: 250, height: 40, backgroundColor: "#F8F8FF", left: 20, borderRadius: 10, paddingLeft: 28 }} />
 
+                    <Icons style={{ position: "absolute", left: 375, zIndex: 1, top: 7, }} name="cart" size={25} color="#363636" />
                     <Icons style={{ position: "absolute", left: 340, zIndex: 1, top: 7, }} name="bell-badge" size={25} color="#363636" />
-                    <Icons style={{ position: "absolute", left: 375, zIndex: 1, top: 7, }} onPress={profile} name="account" size={25} color="#363636" />
                 </View>
 
             </View>
 
-        </SafeAreaView>
+            <ScrollView horizontal>
+                <Text>hifsifds</Text>
+                <Text>hifsifds</Text>
+
+            </ScrollView>
+
+
+
+
+        </View>
 
     );
 }
