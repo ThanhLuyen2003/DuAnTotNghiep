0d608c08acabbe4405f731f8bbce8daabaecaf79
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-10-06 00:39:32 +0700

    Create ComboSalon.js

---
+++ Components/Componentss/ComboSalon.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ComboSalon = () => {
+  return (
+    <View>
+      <Text>ComboSalon</Text>
+    </View>
+  )
+}
+
+export default ComboSalon
+
+const styles = StyleSheet.create({})
