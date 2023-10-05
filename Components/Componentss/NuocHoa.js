22089b110ef6d33721f7f92daeb60cecb29a61be
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-10-06 00:39:29 +0700

    Create NuocHoa.js

---
+++ Components/Componentss/NuocHoa.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const NuocHoa = () => {
+  return (
+    <View>
+      <Text>NuocHoa</Text>
+    </View>
+  )
+}
+
+export default NuocHoa
+
+const styles = StyleSheet.create({})
