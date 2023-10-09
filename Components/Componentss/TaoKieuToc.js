55ea369cdc817ec9cbf8cd585add2f34d13075a3
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-10-09 13:31:28 +0700

    Update TaoKieuToc.js

--- Components/Componentss/TaoKieuToc.js
+++ Components/Componentss/TaoKieuToc.js
@@ -1,83 +1,89 @@
-import { View, Image, StyleSheet, Text, SafeAreaView, FlatList, ActivityIndicator, TouchableOpacity, } from "react-native";
+import { View, Image, StyleSheet, Text, SafeAreaView, FlatList, ActivityIndicator, TouchableOpacity, TouchableHighlight, } from "react-native";
 import { useState } from "react";
 import React from 'react'
 import { height } from "deprecated-react-native-prop-types/DeprecatedImagePropType";
 
 import { FontVariant } from "react-native";
 
-const TaoKieuToc = () => {
-
+const TaoKieuToc = (props) => {
+  const { navigation } = props;
   const [dsProduct, setdsProduct] = useState([])
   const [isLoading, setisLoading] = useState(true);
+
+
+
 
   const getList = async () => {
 
     let apiSalon = 'http://192.168.1.117:3000/apiProduct/productsalon';
 
     try {
-        const response = await fetch(apiSalon);
-        const json = await response.json(); //chuyen du lieu thanh json
+      const response = await fetch(apiSalon);
+      const json = await response.json(); //chuyen du lieu thanh json
 
-        setdsProduct(json);// do du lieu vao state
+      setdsProduct(json);// do du lieu vao state
     } catch (err) {
-        console.error(err);
+      console.error(err);
     } finally {
-        setisLoading(false); // khong con load nua
+      setisLoading(false); // khong con load nua
     }
-}
+  }
 
-React.useEffect(() => {
+  React.useEffect(() => {
 
-  getList();
+    getList();
 
-}, []);
+  }, []);
 
-const numColumns = 2; //
+  const numColumns = 2; //
 
-const renderProductSalon = ({ item }) => {
+  const renderProductSalon = ({ item }) => {
 
-  return (
-    <View >
-      <View style={styles.gridItem}>
-        <Image  style={{width:150,height:250,alignSelf:"center"}} source={{uri:item.avatar}}/>
-           <Text style={{fontSize:15,fontFamily:"OpenSans_Condensed-Bold"}} numberOfLines={2}>{item.name}</Text>       
-           <Text style={{fontSize:15,color:"red"}} >{item.price} Đ</Text> 
-           <Text style={{borderBottomWidth:2,borderColor:"#CD853F",width:110,fontSize:20}}>Xem chi tiết</Text>
-            
+    return (
+      <View >
+        <TouchableHighlight onPress={() => { navigation.navigate("ChiTietItemShop", { avatar: item.avatar, name: item.name, trademark: item.trademark, price: item.price, describe: item.describe, ingredient: item.ingredient, type: item.type }) }}>
+          <View style={styles.gridItem}>
+            <Image style={{ width: 150, height: 250, alignSelf: "center" }} source={{ uri: item.avatar }} />
+            <Text style={{ fontSize: 15 }} numberOfLines={2}>{item.name}</Text>
+            <Text style={{ fontSize: 15, color: "red" }} >{item.price} Đ</Text>
+            {/* <Text style={{borderBottomWidth:2,borderColor:"#CD853F",width:110,fontSize:20}}>Xem chi tiết</Text> */}
+
+          </View>
+        </TouchableHighlight>
+
       </View>
-    </View>
-      
-  );
 
-}
+    );
+
+  }
 
   return (
-    <SafeAreaView style={{height:"88%"}}>
-    <View >
+    <SafeAreaView style={{ height: "88%" }}>
+      <View >
         {
-            (isLoading)
-                ? (<ActivityIndicator style={{ marginTop: 300, }} />)
-                : <FlatList numColumns={numColumns} data={dsProduct} renderItem={renderProductSalon} />
+          (isLoading)
+            ? (<ActivityIndicator style={{ marginTop: 300, }} />)
+            : <FlatList numColumns={numColumns} data={dsProduct} renderItem={renderProductSalon} />
 
         }
-    </View>
-</SafeAreaView>
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default TaoKieuToc
 
 const styles = StyleSheet.create({
-  
+
   gridItem: {
-    backgroundColor:"#fff",
+    backgroundColor: "#fff",
     flex: 1,
-    padding:5,
-    margin:1,
-    width:210,
-    fontSize:12,
-    borderColor:"#F8F8FF",
-    borderWidth:1
+    padding: 5,
+    margin: 1,
+    width: 210,
+    fontSize: 12,
+    borderColor: "#F8F8FF",
+    borderWidth: 1
   },
 
 })
