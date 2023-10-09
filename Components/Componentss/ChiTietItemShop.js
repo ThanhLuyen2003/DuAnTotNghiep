69d245692ae8932d0ffa67c9bade4403bdcf5c74
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-10-09 19:22:36 +0700

    Update ChiTietItemShop.js

--- Components/Componentss/ChiTietItemShop.js
+++ Components/Componentss/ChiTietItemShop.js
@@ -8,8 +8,8 @@ import Ingredient from './Ingredient';
 const topTap = createMaterialTopTabNavigator();
 const ChiTietItemShop = ({ route }) => {
   // console.log(route);
-  let describe=route.params.describe;
-  let ingredient=route.params.ingredient;
+  let describe = route.params.describe;
+  let ingredient = route.params.ingredient;
   return (
 
     <View>
@@ -26,47 +26,47 @@ const ChiTietItemShop = ({ route }) => {
           <View>
             <Text style={{ fontSize: 20, color: "red", fontWeight: "400" }}>{route.params.price} Đ</Text>
             <Text style={{ fontSize: 20, fontWeight: "400" }}>{route.params.name}</Text>
-            <Text style={{ fontSize: 20, fontWeight: "400" }}>{route.params.trademark}</Text>          
+            <Text style={{ fontSize: 20, fontWeight: "400" }}>{route.params.trademark}</Text>
           </View>
 
-          <View style={{width:"95%",height:190,alignSelf:"center",backgroundColor:"#EEEEEE",margin:5,padding:5}}>
+          <View style={{ width: "95%", height: 190, alignSelf: "center", backgroundColor: "#EEEEEE", margin: 5, padding: 5 }}>
 
-            <View style={{flexDirection:"row",justifyContent:"center",alignItems:"center"}}>
+            <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
 
-              <View style={{backgroundColor:"white" ,height:50,width:180,margin:5,padding:5, flexDirection:"row",alignContent:"center",alignItems:'center',borderRadius:5}} >
-                <Icons name='medal-outline' size={20}/>
-                <Text style={{width:140,left:5}}>Cam kết 7 ngày hiệu quả</Text>
+              <View style={{ backgroundColor: "white", height: 50, width: 180, margin: 5, padding: 5, flexDirection: "row", alignContent: "center", alignItems: 'center', borderRadius: 5 }} >
+                <Icons name='medal-outline' size={20} />
+                <Text style={{ width: 140, left: 5 }}>Cam kết 7 ngày hiệu quả</Text>
               </View>
 
-              <View style={{backgroundColor:"white" ,height:50,width:180,margin:5,padding:5, flexDirection:"row",alignContent:"center",alignItems:'center',borderRadius:5}} >
-                <Icons name='star-outline' size={20}/>
-                <Text style={{width:140,left:5}}>Mua 1 hưởng 5 đặc quyền</Text>
-              </View>
-            </View>
-            
-            <View style={{flexDirection:"row",justifyContent:"center",alignItems:"center"}}>
-
-              <View style={{backgroundColor:"white" ,height:50,width:180,margin:5,padding:5, flexDirection:"row",alignContent:"center",alignItems:'center',borderRadius:5}} >
-                <Icons name='currency-usd' size={20}/>
-                <Text style={{width:140,left:5}}>Chính sách hoàn tiền 120%</Text>
-              </View>
-
-              <View style={{backgroundColor:"white" ,height:50,width:180,margin:5,padding:5, flexDirection:"row",alignContent:"center",alignItems:'center',borderRadius:5}} >
-                <Icons name='quality-high' size={20}/>
-                <Text style={{width:140,left:5}}>Chất lượng sản phẩm cao cấp</Text>
+              <View style={{ backgroundColor: "white", height: 50, width: 180, margin: 5, padding: 5, flexDirection: "row", alignContent: "center", alignItems: 'center', borderRadius: 5 }} >
+                <Icons name='star-outline' size={20} />
+                <Text style={{ width: 140, left: 5 }}>Mua 1 hưởng 5 đặc quyền</Text>
               </View>
             </View>
-            
-            <View style={{flexDirection:"row",justifyContent:"center",alignItems:"center"}}>
 
-              <View style={{backgroundColor:"white" ,height:50,width:180,margin:5,padding:5, flexDirection:"row",alignContent:"center",alignItems:'center',borderRadius:5}} >
-                <Icons name='phone-sync' size={20}/>
-                <Text style={{width:140,left:5}}>Tổng đài tư vấn cao cấp</Text>
+            <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+
+              <View style={{ backgroundColor: "white", height: 50, width: 180, margin: 5, padding: 5, flexDirection: "row", alignContent: "center", alignItems: 'center', borderRadius: 5 }} >
+                <Icons name='currency-usd' size={20} />
+                <Text style={{ width: 140, left: 5 }}>Chính sách hoàn tiền 120%</Text>
               </View>
 
-              <View style={{backgroundColor:"white" ,height:50,width:180,margin:5,padding:5, flexDirection:"row",alignContent:"center",alignItems:'center',borderRadius:5}} >
-                <Icons name='shield-sun-outline' size={20}/>
-                <Text style={{width:140,left:5}}>Bảo hành lên đến 12 tháng</Text>
+              <View style={{ backgroundColor: "white", height: 50, width: 180, margin: 5, padding: 5, flexDirection: "row", alignContent: "center", alignItems: 'center', borderRadius: 5 }} >
+                <Icons name='quality-high' size={20} />
+                <Text style={{ width: 140, left: 5 }}>Chất lượng sản phẩm cao cấp</Text>
+              </View>
+            </View>
+
+            <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+
+              <View style={{ backgroundColor: "white", height: 50, width: 180, margin: 5, padding: 5, flexDirection: "row", alignContent: "center", alignItems: 'center', borderRadius: 5 }} >
+                <Icons name='phone-sync' size={20} />
+                <Text style={{ width: 140, left: 5 }}>Tổng đài tư vấn cao cấp</Text>
+              </View>
+
+              <View style={{ backgroundColor: "white", height: 50, width: 180, margin: 5, padding: 5, flexDirection: "row", alignContent: "center", alignItems: 'center', borderRadius: 5 }} >
+                <Icons name='shield-sun-outline' size={20} />
+                <Text style={{ width: 140, left: 5 }}>Bảo hành lên đến 12 tháng</Text>
               </View>
             </View>
 
@@ -85,35 +85,35 @@ const ChiTietItemShop = ({ route }) => {
             </View>
 
           </View>
-{/* tabb */}
-        <View style={{height:500,marginTop:20}} >
-          <topTap.Navigator
-           screenOptions={{
-            tabBarAndroidRipple: { borderless: false },
-            tabBarStyle: {
-               
-              borderWidth: 1,
-              borderColor: "#CCCCCC",
-              borderTopLeftRadius:20,
-              borderTopRightRadius:20
-            },
-            tabBarLabelStyle: {
-              textTransform: "capitalize",
-              fontWeight: "bold"
-            },
-            tabBarIndicatorStyle: {
-              backgroundColor: "#CD853F",
-              height: 5,
-              borderRadius: 5,
-              width: "47%",
-              marginLeft: 5
-            },
-          }}
-          >
-              <topTap.Screen name='Describe' component={Describe} options={{title:"Thông tin sản phẩm"}} initialParams={{describe}} />
-              <topTap.Screen name='Ingredient' component={Ingredient} options={{title:"Thành phần"}} initialParams={{ingredient}}/>
-          </topTap.Navigator>
-        </View>
+          {/* tabb */}
+          <View style={{ height: 300, marginTop: 20 }} >
+            <topTap.Navigator
+              screenOptions={{
+                tabBarAndroidRipple: { borderless: false },
+                tabBarStyle: {
+
+                  borderWidth: 1,
+                  borderColor: "#CCCCCC",
+                  borderTopLeftRadius: 20,
+                  borderTopRightRadius: 20
+                },
+                tabBarLabelStyle: {
+                  textTransform: "capitalize",
+                  fontWeight: "bold"
+                },
+                tabBarIndicatorStyle: {
+                  backgroundColor: "#CD853F",
+                  height: 5,
+                  borderRadius: 5,
+                  width: "47%",
+                  marginLeft: 5
+                },
+              }}
+            >
+              <topTap.Screen name='Describe' component={Describe} options={{ title: "Thông tin sản phẩm" }} initialParams={{ describe }} />
+              <topTap.Screen name='Ingredient' component={Ingredient} options={{ title: "Thành phần" }} initialParams={{ ingredient }} />
+            </topTap.Navigator>
+          </View>
 
         </View>
       </ScrollView>
@@ -127,7 +127,7 @@ const ChiTietItemShop = ({ route }) => {
         <View style={{ backgroundColor: "#CD853F", width: "50%", height: 60, justifyContent: "center", alignItems: "center" }}>
 
           <Pressable onPress={() => { alert("mua ngay") }}><Text style={{ fontWeight: "bold" }}>MUA NGAY</Text></Pressable>
-          <Text style={{top:5}}>Không ưng đổi ngay</Text>
+          <Text style={{ top: 5 }}>Không ưng đổi ngay</Text>
         </View>
       </View>
 
