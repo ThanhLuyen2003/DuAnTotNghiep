e8c847884e8cf79e827230b2630d77380ec8a69b
ThanhLuyen2003 <thanhluyen732003@gmail.com>
2023-10-11 22:10:18 +0700

    Update ComfirmData.js

--- Components/ComfirmData.js
+++ Components/ComfirmData.js
@@ -7,7 +7,7 @@ import { useState } from "react";
 
 const ComfirmData = (props) => {
 
-    const ip = '192.168.1.117';
+    const ip = '192.168.88.103';
 
     const [address, setaddress] = useState();
     const [day, setday] = useState();
