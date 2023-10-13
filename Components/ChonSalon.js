119060e0ebeeddd42ba8657a2b02bbfc5cdc8e50
ThanhLuyen2003 <thanhluyen732003@gmail.com>
2023-10-13 14:26:21 +0700

    Update ChonSalon.js

--- Components/ChonSalon.js
+++ Components/ChonSalon.js
@@ -29,7 +29,7 @@ const ChonSalon = (props) => {
     service = async (item) => {
 
         try {
-
+            await AsyncStorage.setItem('idSalon', item._id);
             await AsyncStorage.setItem('name', item.name);
             await AsyncStorage.setItem('address', item.address);
             await AsyncStorage.setItem('image', item.image);
