a0ac76dd368f9c1f98423bfbbb728ab877e6fa7c
ThanhLuyen2003 <145136443+ThanhLuyen2003@users.noreply.github.com>
2023-10-17 15:33:26 +0700

    Update ManChao.js

--- Components/ManChao.js
+++ Components/ManChao.js
@@ -1,17 +1,56 @@
 import React from "react";
 import { View, Image, StyleSheet, ImageBackground, Text } from "react-native";
 import { useFonts } from 'expo-font';
+import { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 
 const ManChao = (props) => {
+
+    const [userInfor, setUserInfor] = useState({});
+
+    const getLoginInfor = async () => {
+
+        const user = await AsyncStorage.getItem('loginInfo');
+        setUserInfor(JSON.parse(user))
+
+    }
+
+    React.useEffect(() => {
+        const unsubscribe = props.navigation.addListener('focus', () => {
+            // cập nhật giao diện ở đây
+            getLoginInfor();
+
+
+        });
+
+        return unsubscribe;
+    }, [props.navigation]);
+
+
+    setTimeout(() => {
+
+        if (userInfor.name == "") {
+            props.navigation.navigate('GioiThieu')
+
+        } else {
+            props.navigation.navigate('HomeTab')
+
+        }
+
+
+
+
+
+    }, 1000);
+
     const [loaded] = useFonts({
         "DancingScript-Bold": require('../assets/fonts/DancingScript-Bold.ttf'),
     });
     if (!loaded) {
         return undefined;
     }
-    setTimeout(() => {
-        props.navigation.navigate('GioiThieu')
-    }, 1000);
+
 
 
     return (
