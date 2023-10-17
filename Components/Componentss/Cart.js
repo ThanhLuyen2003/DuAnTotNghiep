cde021d412e61e55da76184edd0872b32ec44804
ThanhLuyen2003 <145136443+ThanhLuyen2003@users.noreply.github.com>
2023-10-17 17:39:32 +0700

    Update Cart.js

--- Components/Componentss/Cart.js
+++ Components/Componentss/Cart.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { View, Image, StyleSheet, Text, TextInput, SafeAreaView, ScrollView, ActivityIndicator, FlatList, TouchableOpacity } from "react-native";
+import { View, Image, StyleSheet, Text, TextInput, SafeAreaView, ScrollView, ActivityIndicator, FlatList, TouchableOpacity, Button } from "react-native";
 import Icons from 'react-native-vector-icons/MaterialCommunityIcons'
 import { useState } from 'react';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import CheckBox from "@react-native-community/checkbox";
 
 const Cart = (props) => {
 
     const [isLoading, setisLoading] = useState(true);
-    const [data, setdata] = useState({});
-
-
+    const [data, setdata] = useState([]);
 
     const getList = async () => {
+
 
         let api = 'http://192.168.88.103:3000/getCart/' + props.route.params.id;
 
@@ -29,35 +29,120 @@ const Cart = (props) => {
 
     }
 
-    const renderItem = ({ item }) => {
 
-        //const [soLuong, setSoLuong] = useState(item.quantity);
 
-        var num = Number(item.quantity);
+
+
+    const [tamTinh, setTamTinh] = useState(0);
+
+    selectedItemm = (item, index) => {
+
+        const newData = data.map((e, index) => {
+
+            if (item._id == e._id) {
+                return {
+                    ...e,
+                    selected: !e.selected,
+                }
+            }
+            return {
+                ...e,
+                selected: e.selected
+            }
+
+
+
+        })
+
+        setdata(newData);
+
+
+    }
+
+    const tinhTien = () => {
+        const listSelected = data.filter(item => item.selected == true);
+
+        let price = '0';
+
+
+        listSelected.forEach((item, index) => {
+            price = Number(price) + (Number(item.pricePro) * Number(item.quantity));
+        })
+
+        setTamTinh(price)
+    }
+
+    const renderItem = ({ item, index }) => {
+
 
         const minus = () => {
 
-            if (num == 1) {
+            if (item.quantity == 1) {
                 return;
             }
-            num = num - 1;
 
-            console.log(num);
+            item.quantity = Number(item.quantity) - 1;
+
+            const newData = data.map((e, index) => {
+
+                if (item._id == e._id) {
+                    return {
+                        ...e,
+                    }
+                }
+                return {
+                    ...e,
+                }
+
+
+
+            })
+
+            setdata(newData);
 
         }
 
         const plus = () => {
+
             item.quantity = Number(item.quantity) + 1;
+
+            const newData = data.map((e, index) => {
+
+                if (item._id == e._id) {
+                    return {
+                        ...e,
+                    }
+                }
+                return {
+                    ...e,
+                }
+
+
+
+            })
+
+            setdata(newData);
         }
 
         return (
-            <View style={{ width: '100%', margin: 5, backgroundColor: 'white', padding: 5 }}>
-                <TouchableOpacity>
-                    <View style={{ flexDirection: 'row' }}>
-                        <Image source={{ uri: item.imagePro }} style={{ height: 100, width: 100 }} />
+            <View
+                style={{ width: '100%', margin: 5, padding: 5, backgroundColor: 'white' }}
+            >
+
+                <TouchableOpacity
+                    onPress={() => selectedItemm(item, index)}
+                >
+                    <View style={{ flexDirection: 'row' }}  >
+
+                        <View style={{ marginRight: 5, alignSelf: 'center', height: 20, width: 20, borderWidth: 1 }}>
+                            <Icon style={{ alignSelf: 'center' }} name={(item.selected) ? 'check' : ''} size={20} />
+
+                        </View>
+
+                        <Image source={{ uri: item.imagePro }} style={{ height: 100, width: 100, marginRight: 10 }} />
 
                         <View>
-                            <Text style={{ width: '70%' }}>{item.namePro}</Text>
+                            <Text style={{ width: '60%' }}>{item.namePro}</Text>
                             <Text style={{ fontSize: 18, fontWeight: 'bold' }}>{item.pricePro} đ</Text>
 
                             <View style={{ flexDirection: 'row', marginTop: 20, borderColor: 'black', borderWidth: 1, alignItems: 'center', alignSelf: 'stretch', width: 100, borderRadius: 5, }}>
@@ -82,24 +167,54 @@ const Cart = (props) => {
 
                 </TouchableOpacity>
 
+                <TouchableOpacity>
+
+                    <Icon style={{ position: 'absolute', right: 20, bottom: 10 }} size={30} name="delete" />
+
+                </TouchableOpacity>
+
             </View>
         )
     }
 
     React.useEffect(() => {
+        const unsubscribe = props.navigation.addListener('focus', () => {
+            // cập nhật giao diện ở đây
+            getList();
+        });
 
-        getList();
+        return unsubscribe;
+    }, [props.navigation]);
 
-    }, [])
+
 
     return (
-        <View>
-            {
-                (isLoading)
-                    ? (<ActivityIndicator style={{ marginTop: 300, }} />)
-                    : <FlatList style={{ height: '100%' }} data={data} renderItem={renderItem} />
+        <View style={{ height: '100%' }}>
+            <View style={{ height: '90%' }}>
+                {
+                    (data.length == 0)
+                        ? (<Text style={{ marginTop: 300, alignSelf: 'center' }}>Giỏ hàng rỗng</Text>)
+                        : <FlatList data={data} renderItem={renderItem} />
 
-            }
+                }
+            </View>
+
+            <View style={stylee.order}>
+
+                <TouchableOpacity style={{ height: '100%', backgroundColor: 'aqua', flex: 2 }} onPress={tinhTien}>
+                    <Text style={{ alignSelf: 'center', marginTop: 20 }}>Tính tiền</Text>
+                </TouchableOpacity>
+
+                <View style={{ flex: 2 }}></View>
+
+                <Text style={{ flex: 4, fontSize: 15 }}>Tạm tính: {tamTinh} đ</Text>
+
+                <TouchableOpacity style={{ height: '100%', width: "auto", backgroundColor: '#CD853F', flex: 2 }}>
+                    <Text style={{ alignSelf: 'center', marginTop: 20 }}>Đặt hàng</Text>
+                </TouchableOpacity>
+
+            </View>
+
 
 
         </View>
@@ -107,3 +222,15 @@ const Cart = (props) => {
 }
 
 export default Cart;
+
+const stylee = StyleSheet.create({
+    order: {
+        position: 'absolute',
+        bottom: 0,
+        height: 60,
+        backgroundColor: 'white',
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center'
+    }
+})
