c5c9a49052b7a4450df5479fe6a95bf160620d42
ThanhLuyen2003 <145136443+ThanhLuyen2003@users.noreply.github.com>
2023-10-21 23:01:09 +0700

    Update ChonDichVu.js

--- Components/ChonDichVu.js
+++ Components/ChonDichVu.js
@@ -2,10 +2,6 @@ import { ActivityIndicator, FlatList, Image, StyleSheet, Text, TouchableOpacity,
 import React from 'react'
 import { useState } from "react";
 import { get } from 'react-native/Libraries/TurboModule/TurboModuleRegistry';
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 
 
 const ChonDichVu = (props) => {
