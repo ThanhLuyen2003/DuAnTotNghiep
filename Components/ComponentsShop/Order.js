03b1927241d8e08fa4c3ef02e3922e06bf6d36b7
ThanhLuyen2003 <145136443+ThanhLuyen2003@users.noreply.github.com>
2023-10-30 00:18:25 +0700

    Update Order.js

--- Components/ComponentsShop/Order.js
+++ Components/ComponentsShop/Order.js
@@ -1,10 +1,12 @@
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Order = (props) => {
 
     const [userInfor, setUserInfor] = useState({});
+    const [products, setProducts] = useState([]);
+    const [price, setPrice] = useState();
 
     const getLoginInfor = async () => {
 
@@ -18,6 +20,8 @@ const Order = (props) => {
         const unsubscribe = props.navigation.addListener('focus', () => {
             // cập nhật giao diện ở đây
             getLoginInfor();
+            setProducts(props.route.params.products);
+            setPrice(props.route.params.price);
         });
 
         return unsubscribe;
@@ -33,11 +37,42 @@ const Order = (props) => {
 
                         <Text style={{ fontSize: 20, fontWeight: '500', width: '65%' }} >Thông tin nhận hàng</Text>
 
-                        <TouchableOpacity style={{ alignSelf: 'center', borderWidth: 0.5, padding: 2 }}>
+                        <TouchableOpacity style={{ alignSelf: 'center', borderWidth: 1, padding: 3 }}>
                             <Text>Thay đổi địa chỉ</Text>
                         </TouchableOpacity>
 
                     </View>
+
+                    <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                        <View style={{ width: '30%' }}>
+                            <Text style={{ padding: 5 }}>Họ tên</Text>
+                            <Text style={{ padding: 5 }}>Số điện thoại</Text>
+                            <Text style={{ padding: 5 }}>Email</Text>
+                            <Text style={{ padding: 5 }}>Địa chỉ</Text>
+                        </View>
+
+                        <View>
+                            <Text style={{ padding: 5 }}>{userInfor.name} </Text>
+                            <Text style={{ padding: 5 }}>{userInfor.phone} </Text>
+                            <Text style={{ padding: 5 }}>{userInfor.email} </Text>
+                            <Text style={{ padding: 5, width: '90%' }}>{userInfor.address} </Text>
+
+                        </View>
+
+                    </View>
+
+
+                </View>
+
+                <View style={styles.note}>
+                    <Text style={{ fontSize: 20, fontWeight: '500' }}>Ghi chú đơn hàng</Text>
+                    <TextInput style={styles.input} placeholder='...' placeholderTextColor='black' />
+                </View>
+
+
+                <View style={styles.product}>
+                    <Text style={{ fontSize: 20, fontWeight: '500' }}>Sản phẩm </Text>
+
                 </View>
 
 
@@ -52,6 +87,21 @@ export default Order;
 const styles = StyleSheet.create({
     address: {
         backgroundColor: 'white',
+        padding: 10
+    },
+    note: {
+        backgroundColor: 'white',
+        marginTop: 5,
+        padding: 10,
+    },
+    input: {
+        backgroundColor: '#C0C0C0',
+        height: 'auto',
+        padding: 8,
+    },
+    product: {
+        backgroundColor: 'white',
+        marginTop: 5,
         padding: 10
     }
 
