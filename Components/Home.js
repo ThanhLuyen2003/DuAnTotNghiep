a9334f3b388dd8c0e4782b6ce0f57a89da305e31
ThanhLuyen2003 <145136443+ThanhLuyen2003@users.noreply.github.com>
2023-11-02 00:40:44 +0700

    Update Home.js

--- Components/Home.js
+++ Components/Home.js
@@ -36,6 +36,7 @@ const Home = (props) => {
 
 
 
+
     return (
         <View style={{ height: '90%' }}>
 
@@ -44,7 +45,7 @@ const Home = (props) => {
                 <Image source={{ uri: userInfor.avatar }} style={{ height: 60, width: 60, borderRadius: 50, marginBottom: '5%' }} />
                 <View style={{ alignItems: 'flex-start', marginLeft: 10, marginBottom: '5%' }}>
                     <Text style={{ fontSize: 20, color: 'white' }} >{userInfor.name} </Text>
-                    <Text style={{ color: 'white' }} >Muốn ất ơ đến Fpoly Barber</Text>
+                    <Text style={{ color: 'white' }} >Đẹp như trong mơ đến Fpoly Barber</Text>
                 </View>
 
                 <TouchableOpacity onPress={() => { props.navigation.navigate('Cart', { id: userInfor._id }) }} style={{ position: 'absolute', right: 20 }} >
@@ -107,6 +108,55 @@ const Home = (props) => {
                 </View>
 
                 <Text style={{ fontSize: 25, fontWeight: 'bold', textAlign: 'center', marginBottom: 10 }}>
+                    Trải nghiệm dịch vụ
+                </Text>
+
+                <View style={styles.hot}>
+                    <TouchableOpacity onPress={() => props.navigation.navigate('DatLich')} style={{ flexDirection: 'row', padding: 10, alignItems: 'center' }}>
+                        <Image source={{ uri: 'https://mcdn.coolmate.me/image/October2021/barber.jpg' }} style={{ width: 70, height: 70, borderRadius: 10 }} />
+                        <View style={{ marginLeft: 20, width: '70%' }}>
+                            <Text style={{ fontWeight: '500', fontSize: 18 }}>Cắt, xả, tạo kiểu</Text>
+                            <Text style={{ marginTop: 10, }}>Chỉ từ: 90.000đ</Text>
+                        </View>
+
+
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => props.navigation.navigate('DatLich')} style={{ flexDirection: 'row', padding: 10, alignItems: 'center' }}>
+                        <Image source={{ uri: 'https://media.hasaki.vn/wysiwyg/quynhnhu/Cong-dung-cua-massage-da-nong-3.jpg' }} style={{ width: 70, height: 70, borderRadius: 10 }} />
+                        <View style={{ marginLeft: 20, width: '70%' }}>
+                            <Text style={{ fontWeight: '500', fontSize: 18 }}>Gội Massage dưỡng sinh đá nóng Paradise</Text>
+                            <Text style={{ marginTop: 10, }}>Chỉ từ: 250.000đ</Text>
+                        </View>
+
+
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => props.navigation.navigate('DatLich')} style={{ flexDirection: 'row', padding: 10, alignItems: 'center' }}>
+                        <Image source={{ uri: 'https://storage.beautyfulls.com/uploads-1/thanhtruc/2022/29-06-2022/tay-te-bao-chet-arrahan-nhan-sam-hanbang-soo-peeling-gel-180ml.png' }} style={{ width: 70, height: 70, borderRadius: 10 }} />
+                        <View style={{ marginLeft: 20, width: '70%' }}>
+                            <Text style={{ fontWeight: '500', fontSize: 18 }}>Tẩy da chết nhân sâm trắng da</Text>
+                            <Text style={{ marginTop: 10, }}>Chỉ từ: 60.000đ</Text>
+                        </View>
+
+
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => props.navigation.navigate('DatLich')} style={{ flexDirection: 'row', padding: 10, alignItems: 'center' }}>
+                        <Image source={{ uri: 'https://kenh14cdn.com/2018/5/26/img1220-15273241867941585041203.jpg' }} style={{ width: 70, height: 70, borderRadius: 10 }} />
+                        <View style={{ marginLeft: 20, width: '70%' }}>
+                            <Text style={{ fontWeight: '500', fontSize: 18 }}>Đánh bay mụn cám lột mụn full face</Text>
+                            <Text style={{ marginTop: 10, }}>Chỉ từ: 45.000đ</Text>
+                        </View>
+
+
+                    </TouchableOpacity>
+
+                </View>
+
+
+
+                <Text style={{ fontSize: 25, fontWeight: 'bold', textAlign: 'center', margin: 10 }}>
                     CAM KẾT FPOLY BARBER
                 </Text>
 
@@ -115,13 +165,15 @@ const Home = (props) => {
 
                         <View style={{ flexDirection: 'row', padding: 10, flex: 1 }}>
                             <Icons name="comment" size={50} style={{ color: '#778899' }} />
-                            <Text style={{ position: 'relative', right: 35, top: 8, color: 'white', fontSize: 20, fontWeight: 'bold' }}>
-                                30
-                            </Text>
-                            <View >
+
+                            <View style={{ marginLeft: 10 }} >
                                 <Text style={{ fontWeight: '500', fontSize: 18 }}>30 ngày</Text>
                                 <Text>Đổi & trả {'\n'}miễn phí</Text>
                             </View>
+
+                            <Text style={{ position: 'relative', right: 115, top: 8, color: 'white', fontSize: 20, fontWeight: 'bold' }}>
+                                30
+                            </Text>
                         </View>
 
                         <View style={{ flexDirection: 'row', padding: 10, flex: 1 }}>
@@ -138,13 +190,14 @@ const Home = (props) => {
 
                         <View style={{ flexDirection: 'row', padding: 10, flex: 1 }}>
                             <Icons name="comment" size={50} style={{ color: '#778899' }} />
-                            <Text style={{ position: 'relative', right: 40, top: 8, color: 'white', fontSize: 17, fontWeight: 'bold' }}>
-                                20%
-                            </Text>
-                            <View >
+
+                            <View style={{ marginLeft: 10 }}>
                                 <Text style={{ fontWeight: '500', fontSize: 18 }}>Giảm 20%</Text>
                                 <Text>Nếu chờ đợi {'\n'}quá lâu</Text>
                             </View>
+                            <Text style={{ position: 'relative', right: 133, top: 10, color: 'white', fontSize: 15, fontWeight: 'bold' }}>
+                                20%
+                            </Text>
                         </View>
 
                         <View style={{ flexDirection: 'row', padding: 10, flex: 1 }}>
@@ -179,7 +232,7 @@ export default Home;
 
 const styles = StyleSheet.create({
     con2: {
-        height: '83.5%',
+        height: '84%',
         width: '100%',
         backgroundColor: 'white',
         borderTopLeftRadius: 20,
@@ -203,7 +256,8 @@ const styles = StyleSheet.create({
         width: '90%',
         alignSelf: 'center',
         backgroundColor: '#EEEEEE',
-        borderRadius: 10
+        borderRadius: 10,
+
 
     }
 
