67706711494fc3ff46c3d674d25611607091e577
ThanhLuyen2003 <145136443+ThanhLuyen2003@users.noreply.github.com>
2023-11-01 17:44:37 +0700

    Update Home.js

--- Components/Home.js
+++ Components/Home.js
@@ -1,24 +1,176 @@
-import React from "react";
-import { View, Image, StyleSheet, Text, TextInput, SafeAreaView, ScrollView, ImageBackground } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Text, TextInput, SafeAreaView, ScrollView, ImageBackground, Image, TouchableOpacity } from "react-native";
 import Icons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Onboarding from 'react-native-onboarding-swiper';
 import { height } from "deprecated-react-native-prop-types/DeprecatedImagePropType";
+import LinearGradient from "react-native-linear-gradient";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Home = (props) => {
-    const { navigation } = props
-    const profile = () => {
-        navigation.navigate("Profile")
+
+    const [userInfor, setUserInfor] = useState({});
+    const [saveImage, setsaveImage] = useState({});
+
+
+    const getLoginInfor = async () => {
+
+        const user = await AsyncStorage.getItem('loginInfo');
+        const m_saveImage = await AsyncStorage.getItem('savedImage')
+
+        setUserInfor(JSON.parse(user))
+        setsaveImage(m_saveImage);
+
     }
+
+    React.useEffect(() => {
+        const unsubscribe = props.navigation.addListener('focus', () => {
+            // cập nhật giao diện ở đây
+            getLoginInfor();
+
+
+        });
+
+        return unsubscribe;
+    }, [props.navigation]);
+
+
+
+
     return (
-        <SafeAreaView>
+        <View style={{ height: '90%' }}>
 
+            <View style={{ height: '20%', width: '100%', backgroundColor: "#778899", flexDirection: 'row', alignItems: 'center', padding: 20 }}>
 
+                <Image source={{ uri: userInfor.avatar }} style={{ height: 60, width: 60, borderRadius: 50, marginBottom: '5%' }} />
+                <View style={{ alignItems: 'flex-start', marginLeft: 10, marginBottom: '5%' }}>
+                    <Text style={{ fontSize: 20, color: 'white' }} >{userInfor.name} </Text>
+                    <Text style={{ color: 'white' }} >Muốn ất ơ đến Fpoly Barber</Text>
+                </View>
 
-            <View style={{ height: '70%', width: '100%', backgroundColor: 'yellow' }}>
+                <TouchableOpacity onPress={() => { props.navigation.navigate('Cart', { id: userInfor._id }) }} style={{ position: 'absolute', right: 20 }} >
+                    <Icons name="cart" size={25} color="white" />
+                </TouchableOpacity>
+
+                <TouchableOpacity style={{ position: 'absolute', right: 60 }}>
+                    <Icons name="account" size={25} color="white" />
+                </TouchableOpacity>
+
 
             </View>
 
-        </SafeAreaView>
+            <ScrollView style={styles.con2}>
+
+                <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 20 }}>
+                    <TouchableOpacity style={{ alignItems: 'center' }}>
+                        <View style={styles.but}>
+                            <Image source={require('../Images/imgHome/datlich.png')} style={{ width: 30, height: 30 }} />
+                        </View>
+                        <Text>Đặt lịch</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={{ alignItems: 'center' }}>
+                        <View style={styles.but}>
+                            <Image source={require('../Images/imgHome/lichsucat.png')} style={{ width: 30, height: 30 }} />
+                        </View>
+                        <Text>Lịch sử{"\n"}   cắt</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={{ alignItems: 'center' }}>
+                        <View style={styles.but}>
+                            <Image source={require('../Images/imgHome/donhang.png')} style={{ width: 30, height: 30 }} />
+                        </View>
+                        <Text>Đơn hàng</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={{ alignItems: 'center' }}>
+                        <View style={styles.but}>
+                            <Image source={require('../Images/imgHome/hethong.png')} style={{ width: 30, height: 30 }} />
+                        </View>
+                        <Text>Hệ thống{"\n"}   salon</Text>
+                    </TouchableOpacity>
+                </View>
+
+
+
+                <ScrollView horizontal showsHorizontalScrollIndicator={false} pagingEnabled>
+
+                    <Image source={require('../Images/imgHome/matxa.jpg')} style={styles.img} />
+                    <Image source={require('../Images/imgHome/cattoc.jpg')} style={styles.img} />
+                    <Image source={require('../Images/imgHome/lotmun.jpg')} style={styles.img} />
+                    <Image source={require('../Images/imgHome/nhuomtoc.jpg')} style={styles.img} />
+                    <Image source={require('../Images/imgHome/banner1.png')} style={styles.img} />
+
+                </ScrollView>
+
+                <View style={{ position: 'relative', width: '100%', alignItems: 'center', bottom: 40, height: 30 }}>
+                    <Text style={{ fontSize: 30, color: 'white' }}>•••</Text>
+                </View>
+
+                <Text style={{ fontSize: 25, fontWeight: 'bold', textAlign: 'center', marginBottom: 10 }}>
+                    CAM KẾT FPOLY BARBER
+                </Text>
+
+                <View style={styles.hot}>
+                    <View style={{ flexDirection: 'row-reverse' }}>
+
+                        <View style={{ flexDirection: 'row', padding: 10, flex: 1 }}>
+                            <Icons name="comment" size={50} style={{ color: '#778899' }} />
+                            <Text style={{ position: 'relative', right: 35, top: 8, color: 'white', fontSize: 20, fontWeight: 'bold' }}>
+                                30
+                            </Text>
+                            <View >
+                                <Text style={{ fontWeight: '500', fontSize: 18 }}>30 ngày</Text>
+                                <Text>Đổi & trả {'\n'}miễn phí</Text>
+                            </View>
+                        </View>
+
+                        <View style={{ flexDirection: 'row', padding: 10, flex: 1 }}>
+                            <Icons name="comment" size={50} style={{ color: '#778899' }} />
+                            <Text style={{ position: 'relative', right: 30, top: 8, color: 'white', fontSize: 20, fontWeight: 'bold' }}>7</Text>
+                            <View >
+                                <Text style={{ fontWeight: '500', fontSize: 18 }}>7 ngày</Text>
+                                <Text>Chỉnh sửa tóc {'\n'}miễn phí</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View style={{ flexDirection: 'row-reverse' }}>
+
+                        <View style={{ flexDirection: 'row', padding: 10, flex: 1 }}>
+                            <Icons name="comment" size={50} style={{ color: '#778899' }} />
+                            <Text style={{ position: 'relative', right: 40, top: 8, color: 'white', fontSize: 17, fontWeight: 'bold' }}>
+                                20%
+                            </Text>
+                            <View >
+                                <Text style={{ fontWeight: '500', fontSize: 18 }}>Giảm 20%</Text>
+                                <Text>Nếu chờ đợi {'\n'}quá lâu</Text>
+                            </View>
+                        </View>
+
+                        <View style={{ flexDirection: 'row', padding: 10, flex: 1 }}>
+                            <Icons name="comment" size={50} style={{ color: '#778899' }} />
+                            <Text style={{ position: 'relative', right: 30, top: 8, color: 'white', fontSize: 20, fontWeight: 'bold' }}>
+                                7
+                            </Text>
+                            <View >
+                                <Text style={{ fontWeight: '500', fontSize: 18 }}>7 ngày</Text>
+                                <Text>Bảo hành Uốn {'\n'}Nhuộm</Text>
+                            </View>
+                        </View>
+                    </View>
+
+
+
+
+                </View>
+
+                <View style={{ height: 100 }}></View>
+
+
+
+            </ScrollView>
+
+        </View>
 
     );
 }
@@ -26,113 +178,33 @@ const Home = (props) => {
 export default Home;
 
 const styles = StyleSheet.create({
-    text: {
-        fontSize: 20,
-        fontWeight: "bold",
-        textAlign: "center",
+    con2: {
+        height: '83.5%',
+        width: '100%',
+        backgroundColor: 'white',
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        position: 'absolute',
+        top: "17%"
     },
-    verticalLayout: {
-        flexDirection: "column",
-    },
-    horizontalLayout: {
-        flexDirection: "row",
-        marginLeft: 40,
-        marginTop: 20,
-    },
-    item: {
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        marginLeft: 20,
-    },
-    iconContainer: {
-        backgroundColor: 'gray',
+    but: {
+        backgroundColor: '#778899',
         padding: 10,
-        borderRadius: 50
-    },
-    icon: {
-        width: 30,
-        height: 30,
-    },
-    itemText: {
-        marginTop: 5,
-        textAlign: "center",
-    },
-    menuContainer: {
-        alignItems: 'center',
-        paddingVertical: 10,
-    },
-    viewTrainghiem: {
-        backgroundColor: "#FFFFFF",
-        width: 350,
-        height: 300,
-        borderRadius: 10,
-        marginLeft: 20,
-        marginTop: 15,
-        marginBottom: 15,
-    },
-    viewTopsp: {
-        textAlign: "center",
-        backgroundColor: "#FFFFFF",
-        width: 130,
-        height: 190,
-        marginLeft: 20,
-        marginTop: 15,
-        marginBottom: 15,
-    },
+        borderRadius: 50,
+        width: 50,
+        height: 50
 
-    viewCamket: {
-        backgroundColor: "#FFFFFF",
-        width: 350,
-        height: 120,
-        marginTop: 20,
-        marginLeft: 20,
     },
+    img: {
+        width: 392, height: 140, margin: 10,
+        borderRadius: 10
+    },
+    hot: {
+        width: '90%',
+        alignSelf: 'center',
+        backgroundColor: '#EEEEEE',
+        borderRadius: 10
 
-    viewminitopsp: {
-        height: 25,
-        width: 130,
-        backgroundColor: "rgba(251, 154, 69, 0.8)",
-    },
-    trainghiemSrcoll: {
-        textAlign: "center",
-    },
-    menuItem: {
-        flex: 1,
-        marginRight: 10,
-    },
-    menuItemTrainghiem: {
-        flex: 2,
-        marginRight: 10,
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "flex-end",
-    },
-    menuItemImage: {
-        marginLeft: 20,
-        width: 350,
-        height: 100,
-        borderRadius: 10,
-    },
-    menuTrainghiem: {
-        marginLeft: 20,
-        width: 100,
-        height: 100,
-        marginTop: 15,
-        borderRadius: 10,
-    },
-    textTrainghiem: {
-        fontSize: 17,
-        marginLeft: 5,
-    },
-    textTrainghiem2: {
-        fontSize: 14,
-        marginLeft: 5,
-    },
-    Imagetopsp: {
-        marginLeft: 15,
-        width: 100,
-        height: 100,
-    },
+    }
 
 });
