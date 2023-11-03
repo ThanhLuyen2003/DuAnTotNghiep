c982fd2540a710d81d434ecfa86d2f76501f30e5
ThanhLuyen2003 <145136443+ThanhLuyen2003@users.noreply.github.com>
2023-11-03 16:08:39 +0700

    Create DaGiao.js

---
+++ Components/ComponentsDonHang/DaGiao.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DaGiao = () => {
+    return (
+        <View>
+            <Text>ChamSocDa</Text>
+        </View>
+    )
+}
+
+export default DaGiao
+
+const styles = StyleSheet.create({})
