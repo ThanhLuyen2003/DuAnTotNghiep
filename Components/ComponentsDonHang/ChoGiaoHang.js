9b602f77576f35563492d2072a2d032d42f08a64
ThanhLuyen2003 <145136443+ThanhLuyen2003@users.noreply.github.com>
2023-11-03 16:08:35 +0700

    Create ChoGiaoHang.js

---
+++ Components/ComponentsDonHang/ChoGiaoHang.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const ChoGiaoHang = (props) => {
+
+
+
+    return (
+        <View>
+            <Text>ChamSocDa</Text>
+        </View>
+    )
+}
+
+export default ChoGiaoHang
+
+const styles = StyleSheet.create({})
