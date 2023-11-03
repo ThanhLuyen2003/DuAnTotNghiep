0e0cfb310e1cbe8fdf4312f12393d42be6278501
ThanhLuyen2003 <145136443+ThanhLuyen2003@users.noreply.github.com>
2023-11-03 16:08:42 +0700

    Create DaHuy.js

---
+++ Components/ComponentsDonHang/DaHuy.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DaHuy = () => {
+    return (
+        <View>
+            <Text>ChamSocDa</Text>
+        </View>
+    )
+}
+
+export default DaHuy
+
+const styles = StyleSheet.create({})
