49eb759a6deec8075927699287f41c679cb0ebed
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-11-04 10:15:20 +0700

    Update KhamPha.js

--- Components/KhamPha.js
+++ Components/KhamPha.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Text, TextInput, SafeAreaView, ScrollView, ImageBackground, Image, TouchableOpacity } from "react-native";
+import { View, StyleSheet, Text, TextInput, SafeAreaView, ScrollView, ImageBackground, Image, TouchableOpacity, TouchableHighlight } from "react-native";
 import Icons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Onboarding from 'react-native-onboarding-swiper';
 import LinearGradient from "react-native-linear-gradient";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 
 const KhamPha = (props) => {
     const [userInfor, setUserInfor] = useState({});
     const [saveImage, setsaveImage] = useState({});
+    const [likeCount, setLikeCount] = useState(0);
+    const [isLiked, setIsLiked] = useState(false);
+    const [isPressed, setIsPressed] = useState(false);
+
     const getLoginInfor = async () => {
 
         const user = await AsyncStorage.getItem('loginInfo');
@@ -30,8 +35,17 @@ const KhamPha = (props) => {
         return unsubscribe;
     }, [props.navigation]);
 
-  return (
-    <View style={{ height: '90%' }}>
+    const yeuThich =()=>{
+            if(isLiked){
+                setLikeCount(likeCount -1)
+            }else{
+                setLikeCount(likeCount +1)
+            }
+            setIsLiked(!isLiked);
+            setIsPressed(!isPressed)
+    }
+    return (
+        <View style={{ height: '90%' }}>
 
             <View style={{ height: '20%', width: '100%', backgroundColor: "#778899", flexDirection: 'row', alignItems: 'center', padding: 20 }}>
 
@@ -53,12 +67,33 @@ const KhamPha = (props) => {
             </View>
 
             <ScrollView style={styles.con2}>
+                <View style={{ flex: 1, alignItems: "center" }}>
+                    <Text>CÙNG 30SHINE KHÁM PHÁ</Text>
+                    <Text style={{ fontSize: 18, fontWeight: "bold" }}>XU HƯỚNG TÓC HOT NHẤT</Text>
+                    <View style={{ width: "100%", height: 45, backgroundColor: "#C0C0C0", padding: 10, borderRadius: 10, flexDirection: "row", alignItems: "center" }}>
+                        <Icon name="robot-love" size={20} />
+                        <Text style={{ marginLeft: 10 }}>Đã thích</Text>
+                        <Text style={{ marginLeft: "auto", backgroundColor: "yellow", width: 20, height: 20, paddingLeft: 6, borderRadius: 50 }}>{likeCount}</Text>
+                    </View>
+                </View>
+                <View style={{ flexDirection: "row", justifyContent: "center", top: 10 }}>
 
-          
+                    <Image style={{ width: "48%", height: 200, right: 5, borderRadius: 10, opacity: 0.8 }} source={{ uri: "https://classic.vn/wp-content/uploads/2022/09/classicvn-kieu-toc-side-part-vuot-ru-dep-d.png" }} />
+                    <Text style={{ position: "absolute", left: 25, top: 160, color: "white" }}>Phá cách với kiểu tóc...</Text>
+                    <TouchableHighlight onPress={yeuThich}>
+                        <Icons name="robot-love-outline" size={25} style={{ position: "absolute", right:20, top: 10, width: 40, height: 40,  backgroundColor: isPressed ? 'yellow' : '#CCCCCC', paddingLeft: 7, paddingTop: 6, borderRadius: 50 }} color={"white"} />
+                    </TouchableHighlight>
+
+                    <Image style={{ width: "48%", height: 200, left: 5, borderRadius: 10, opacity: 0.8 }} source={{ uri: "https://dungtranacademy.vn/wp-content/uploads/2021/09/cham-soc-toc-hay-cho-nam-gioi.jpg" }} />
+                    <Text style={{ position: "absolute", right: 25, top: 160, color: "white" }}>4 Mẹo Giữ Form Tóc...</Text>
+                    <TouchableHighlight onPress={yeuThich}>
+                        <Icons name="robot-love-outline" size={25} style={{ position: "absolute", right:20, top: 10, width: 40, height: 40,  backgroundColor: isPressed ? 'yellow' : '#CCCCCC', paddingLeft: 7, paddingTop: 6, borderRadius: 50 }} color={"white"} />
+                    </TouchableHighlight>
+                </View>
             </ScrollView>
 
         </View>
-  )
+    )
 }
 
 export default KhamPha
@@ -71,7 +106,8 @@ const styles = StyleSheet.create({
         borderTopLeftRadius: 20,
         borderTopRightRadius: 20,
         position: 'absolute',
-        top: "17%"
+        top: "17%",
+        padding: 10
     },
     but: {
         backgroundColor: '#778899',
