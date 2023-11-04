d87d374fc1b34cccdcaa964e10158457491e120d
ThanhLuyen2003 <145136443+ThanhLuyen2003@users.noreply.github.com>
2023-11-04 11:28:09 +0700

    Update ChoGiaoHang.js

--- Components/ComponentsDonHang/ChoGiaoHang.js
+++ Components/ComponentsDonHang/ChoGiaoHang.js
@@ -1,14 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ActivityIndicator, FlatList, Image, StyleSheet, Text, View } from 'react-native'
 import React, { useState } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const ChoGiaoHang = (props) => {
 
+    const [donhang, setDonhang] = useState([]);
+    const [isLoading, setisLoading] = useState(true);
+
+    const ip = "192.168.0.103";
+
+    const id = props.route.params.id;
+
+
+    const getList = async () => {
+
+
+        let api = 'http://' + ip + ':3000/getOrder/' + id + '/Chờ giao hàng';
+
+
+        try {
+            const response = await fetch(api);
+            const json = await response.json(); //chuyen du lieu thanh json
+
+            setDonhang(json);// do du lieu vao state
+        } catch (err) {
+            console.error(err);
+        } finally {
+            setisLoading(false); // khong con load nua
+        }
+    }
+
+    React.useEffect(() => {
+        const unsubscribe = props.navigation.addListener('focus', () => {
+            // cập nhật giao diện ở đây
+            getList();
+        });
+
+        return unsubscribe;
+    }, [props.navigation]);
+
+    console.log(donhang);
 
 
     return (
         <View>
-            <Text>ChamSocDa</Text>
+            {isLoading
+                ? <ActivityIndicator style={{ alignSelf: "center", marginTop: 200 }} />
+                : donhang.length == 0
+                    ? <Image style={{ width: 100, height: 100, alignSelf: "center", marginTop: 200 }} source={require('./document_icon.png')} />
+                    : <FlatList />
+            }
         </View>
     )
 }
