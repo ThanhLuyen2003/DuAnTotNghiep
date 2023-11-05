23a9a1b43a536826ee15a452602e97a5152cd91a
ThanhLuyen2003 <145136443+ThanhLuyen2003@users.noreply.github.com>
2023-11-05 23:48:41 +0700

    Update ChoLayHang.js

--- Components/ComponentsDonHang/ChoLayHang.js
+++ Components/ComponentsDonHang/ChoLayHang.js
@@ -1,4 +1,4 @@
-import { ActivityIndicator, FlatList, Image, StyleSheet, Text, View } from 'react-native'
+import { ActivityIndicator, FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -7,7 +7,7 @@ const ChoLayHang = (props) => {
     const [donhang, setDonhang] = useState([]);
     const [isLoading, setisLoading] = useState(true);
 
-    const ip = "192.168.0.103";
+    const ip = "192.168.88.101";
 
     const id = props.route.params.id;
 
@@ -31,28 +31,47 @@ const ChoLayHang = (props) => {
     }
 
     React.useEffect(() => {
-        const unsubscribe = props.navigation.addListener('focus', () => {
-            // cập nhật giao diện ở đây
-            getList();
-        });
+        getList();
 
-        return unsubscribe;
-    }, [props.navigation]);
+    }, []);
 
     const renderItem = ({ item }) => {
 
         const product = item.products;
 
-        console.log(product[0].name);
-
         return (
-            <View>
+            <TouchableOpacity
+                style={{ backgroundColor: 'white', marginBottom: 10, padding: 10 }}
+                onPress={() => props.navigation.navigate('ChiTietDonHang',
+                    { name: item.nameU, address: item.addressU, phone: item.phoneU, message: item.message, price: item.price, time: item.time, product: product, id: item._id })}
+            >
 
-                <Text>{item.nameU}</Text>
+                <Text style={styles.status}>{item.status}</Text>
 
+                <View style={{ flexDirection: 'row' }}>
+                    <Image source={{ uri: product[0].image }} style={{ height: 100, width: 100 }} />
 
+                    <View style={{ alignSelf: 'center', width: "100%", marginLeft: 15 }}>
 
-            </View>
+                        <Text style={{ fontSize: 15, width: "75%", }}>{product[0].name}</Text>
+                        <Text style={{ fontSize: 15, fontWeight: 'bold' }}>{product[0].price} đ</Text>
+                        <Text style={{ fontSize: 15, }}> x{product[0].quantity}</Text>
+
+                    </View>
+                </View>
+
+                <View style={{ flexDirection: 'row' }}>
+                    <View style={{ width: '50%' }}>
+                        <Text>Cùng {product.length - 1} sản phẩm khác nữa </Text>
+
+                    </View>
+                    <View style={{ flexDirection: 'row', }}>
+                        <Text>Thành tiền:</Text>
+                        <Text style={{ color: 'red' }}> {item.price} đ</Text>
+                    </View>
+                </View>
+
+            </TouchableOpacity>
         )
 
     }
@@ -76,4 +95,9 @@ const ChoLayHang = (props) => {
 
 export default ChoLayHang
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    status: {
+        alignSelf: 'flex-end', marginRight: 30,
+        color: 'red',
+    }
+})
