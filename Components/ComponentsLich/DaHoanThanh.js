59a3bb2cb0445332a0acd8ac6ef666542ca73ae1
ThanhLuyen2003 <145136443+ThanhLuyen2003@users.noreply.github.com>
2023-11-06 17:30:48 +0700

    Create DaHoanThanh.js

---
+++ Components/ComponentsLich/DaHoanThanh.js
@@ -0,0 +1,133 @@
+import { ActivityIndicator, FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+
+const DaHoanThanh = (props) => {
+
+    const ip = "192.168.0.102";
+
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [dataLich, setDataLich] = useState([]);
+
+    const id = props.route.params.id;
+
+    const getList = async () => {
+
+
+        let api = 'http://' + ip + ':3000/getBill/' + id + '/Đã hoàn thành';
+
+
+        try {
+            const response = await fetch(api);
+            const json = await response.json(); //chuyen du lieu thanh json
+
+            setDataLich(json);// do du lieu vao state
+        } catch (err) {
+            console.error(err);
+        } finally {
+            setIsLoading(false); // khong con load nua
+        }
+    }
+
+    React.useEffect(() => {
+        // cập nhật giao diện ở đây
+        getList();
+
+
+    }, []);
+
+    const renderItem = ({ item }) => {
+        return (
+            <View style={{ borderWidth: 1, marginBottom: 10, borderRadius: 20 }}>
+                <View style={{ padding: 10, flexDirection: 'row', alignItems: 'center' }}>
+                    <Text style={{ fontWeight: '600' }}>{item.day} </Text>
+                    <Text style={{ fontWeight: '600', width: '45%' }}>  /   {item.hour} </Text>
+
+                    <View style={styles.status}>
+                        <Text style={{ color: 'white' }}>{item.status} </Text>
+
+                    </View>
+                </View>
+
+                <View style={styles.con1}>
+
+                    <Image source={{ uri: item.imageSalon }} style={{ width: 100, height: 100, borderRadius: 10 }} />
+
+                    <View style={{ marginLeft: 10 }}>
+
+                        <Text style={{ marginTop: 10 }}>{item.nameSalon}</Text>
+                        <Text style={{ marginTop: 10, width: '75%' }}>{item.addressSalon}</Text>
+                        <Text style={{ marginTop: 10 }}>{item.phone}</Text>
+
+                    </View>
+
+
+                </View>
+
+                <View style={styles.con2}>
+
+                    <TouchableOpacity style={styles.but1}>
+                        <Text style={{ marginTop: 8, color: 'white' }}>Xem hóa đơn</Text>
+                    </TouchableOpacity>
+
+                </View>
+
+
+
+            </View>
+        )
+    }
+
+
+    return (
+        <View style={{ padding: 10 }}>
+
+            {isLoading
+                ? <ActivityIndicator style={{ alignSelf: "center", marginTop: 200 }} />
+                : dataLich.length == 0
+                    ? <Image style={{ width: 100, height: 100, alignSelf: "center", marginTop: 200 }} source={require('../ComponentsDonHang/document_icon.png')} />
+                    : <FlatList data={dataLich} renderItem={renderItem} />
+            }
+
+        </View>
+    )
+}
+
+export default DaHoanThanh;
+
+const styles = StyleSheet.create({
+    status: {
+        backgroundColor: 'green',
+        padding: 5,
+        width: 120,
+        alignItems: 'center',
+        borderRadius: 5
+    },
+    con1: {
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        flexDirection: 'row',
+        padding: 10
+    },
+    con2: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        height: 50,
+        padding: 10
+    },
+    but1: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: 'green',
+        height: 35,
+        borderRadius: 20
+    },
+    but2: {
+        flex: 1,
+        alignItems: 'center',
+        borderColor: 'green',
+        height: 35,
+        borderRadius: 20,
+        borderWidth: 1
+    }
+})
