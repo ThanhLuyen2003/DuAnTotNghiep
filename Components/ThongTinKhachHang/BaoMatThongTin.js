923b6337298cc1ae5b23cbe8782db544f4a7345a
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-11-08 15:18:35 +0700

    Create BaoMatThongTin.js

---
+++ Components/ThongTinKhachHang/BaoMatThongTin.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const BaoMatThongTin = () => {
+  return (
+    <View>
+      <Text>BaoMatThongTin</Text>
+    </View>
+  )
+}
+
+export default BaoMatThongTin
+
+const styles = StyleSheet.create({})
