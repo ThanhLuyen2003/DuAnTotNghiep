08ed55ed9bb5927d3c773c8240fdc0826c39d0a7
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-11-08 15:18:38 +0700

    Create CamKetFpoly.js

---
+++ Components/ThongTinKhachHang/CamKetFpoly.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CamKetFpoly = () => {
+  return (
+    <View>
+      <Text>CamKetFpoly</Text>
+    </View>
+  )
+}
+
+export default CamKetFpoly
+
+const styles = StyleSheet.create({})
