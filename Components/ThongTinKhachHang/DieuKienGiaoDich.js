90acdfb991bac05b653b98984fd64e82472915f1
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-11-08 15:18:42 +0700

    Create DieuKienGiaoDich.js

---
+++ Components/ThongTinKhachHang/DieuKienGiaoDich.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DieuKienGiaoDich = () => {
+  return (
+    <View>
+      <Text>DieuKienGiaoDich</Text>
+    </View>
+  )
+}
+
+export default DieuKienGiaoDich
+
+const styles = StyleSheet.create({})
