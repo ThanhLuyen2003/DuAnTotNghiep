9b7a3dcac4b9398be69d48cb494592a5c9e00fa7
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-11-09 11:19:48 +0700

    Update VeChungToi.js

--- Components/ThongTinKhachHang/VeChungToi.js
+++ Components/ThongTinKhachHang/VeChungToi.js
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const VeChungToi = () => {
+const VeChungToi = (props) => {
   return (
     <View>
-      <Text>VeChungToi</Text>
+      <View style={{width:"100%",padding:20}}>
+        <Image source={{uri:"https://i.pinimg.com/564x/0b/b7/af/0bb7afd6a2596aadc215cd7ce77e6000.jpg"}}  width={"100%"} height={150} style={{opacity:0.5,borderRadius:20}}/>
+      </View>
     </View>
   )
 }
