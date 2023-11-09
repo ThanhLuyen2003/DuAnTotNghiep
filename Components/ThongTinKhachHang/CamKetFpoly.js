cd26625ae53d3bd78ab902c17150c0b8ae530a55
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-11-09 09:44:09 +0700

    Update CamKetFpoly.js

--- Components/ThongTinKhachHang/CamKetFpoly.js
+++ Components/ThongTinKhachHang/CamKetFpoly.js
@@ -1,11 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const CamKetFpoly = () => {
   return (
-    <View>
-      <Text>CamKetFpoly</Text>
-    </View>
+   <ScrollView style={{width:"100%",height:"100%"}}>
+      <View>
+
+      </View>
+   </ScrollView>
   )
 }
 
