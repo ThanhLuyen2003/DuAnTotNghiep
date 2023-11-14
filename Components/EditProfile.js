9523d3e08e778cb0b251935582ff8b105cbf389a
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-11-14 19:25:19 +0700

    Update EditProfile.js

--- Components/EditProfile.js
+++ Components/EditProfile.js
@@ -7,6 +7,7 @@ import DatePicker from '@react-native-community/datetimepicker'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
+import { useEffect } from 'react'
 import ip from '../IP'
 const EditProfile = (props) => {
     const [userInfo, setuserInfo] = useState({
@@ -46,69 +47,128 @@ const EditProfile = (props) => {
 
     }, []);
 
-    const saveImageToStorage = async (imageBase64) => {
+    const saveImageToStorage = async (imageBase64, shouldSave) => {
         try {
-            if (imageBase64) {
+            if (imageBase64 && shouldSave) {
                 await AsyncStorage.setItem('savedImage', imageBase64);
                 console.log('Đã lưu ảnh vào Storage.');
             } else {
-                console.log('Dữ liệu ảnh không tồn tại.');
+                console.log('Dữ liệu ảnh không tồn tại hoặc không cần lưu.');
             }
         } catch (error) {
             console.log('Lỗi khi lưu ảnh vào Storage:', error);
         }
     };
 
+    // const pickImage = async () => {
+    //     // Đọc ảnh từ thư viện thì không cần khai báo quyền
+    //     let result = await ImagePicker.launchImageLibraryAsync({
+    //         mediaTypes: ImagePicker.MediaTypeOptions.All,
+    //         allowsEditing: true,
+    //         aspect: [4, 3], // khung view cắt ảnh 
+    //         quality: 1,
+    //     });
+    
+    //     console.log(result);
+    
+    //     if (!result.cancelled) {
+    //         if (result.assets.length > 0 && result.assets[0].uri) {
+    //             setimg_source(result.assets[0].uri);
+    //             let _uri = result.assets[0].uri;  // địa chỉ file ảnh đã chọn
+    //             let file_ext = _uri.substring(_uri.lastIndexOf('.') + 1); // lấy đuôi file
+    //             FileSystem.readAsStringAsync(result.assets[0].uri, { encoding: 'base64' })
+    //                 .then((res) => {
+    //                     // phải nối chuỗi với tiền tố data image
+    //                     setiimg_base64("data:image/" + file_ext + ";base64," + res);
+    //                     console.log(img_base64);
+    
+    //                     // upload ảnh lên api thì dùng PUT có thể viết ở đây
+    //                     let url_api = "http://" + ip + ":3000/apiuser/updateAvatar/" + userInfo._id
+    //                     let obj1 = { avatar: img_base64 }
+    //                     fetch(url_api, {
+    //                         method: 'PUT',
+    //                         headers: {
+    //                             Accept: 'application/json',
+    //                             'Content-Type': 'application/json',
+    //                         },
+    //                         body: JSON.stringify(obj1),
+    //                     }).then(async (res) => {
+    //                         if (res.status === 200) {
+    //                             // Save the image to storage
+    //                             saveImageToStorage(img_base64, true);
+    //                         } else {
+    //                             alert("Có lỗi xảy ra!")
+    //                             console.log(res);
+    //                             return res;
+    //                         }
+    //                     }).catch((err) => {
+    //                         console.log(err);
+    //                     });
+    //                 });
+    //         } else {
+    //             // Image selection canceled, do not save
+    //             saveImageToStorage(null, false);
+    //         }
+    //     }
+    // }
     const pickImage = async () => {
-
-        // Đọc ảnh từ thư viện thì không cần khai báo quyền
         let result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.All,
             allowsEditing: true,
-            aspect: [4, 3], // khung view cắt ảnh 
-            quality: 1,
+            aspect: [4, 3],
+            quality: 0.0001,
         });
+    
         console.log(result);
+    
         if (!result.cancelled) {
             if (result.assets.length > 0 && result.assets[0].uri) {
                 setimg_source(result.assets[0].uri);
-                let _uri = result.assets[0].uri;  // địa chỉ file ảnh đã chọn
-                let file_ext = _uri.substring(_uri.lastIndexOf('.') + 1); // lấy đuôi file
+                let _uri = result.assets[0].uri;
+                let file_ext = _uri.substring(_uri.lastIndexOf('.') + 1);
+    
                 FileSystem.readAsStringAsync(result.assets[0].uri, { encoding: 'base64' })
                     .then((res) => {
-                        // phải nối chuỗi với tiền tố data image
                         setiimg_base64("data:image/" + file_ext + ";base64," + res);
-
                         console.log(img_base64);
-
-                        // upload ảnh lên api thì dùng PUT có thể viết ở đây
-                        let url_api = "http://" + ip + ":3000/apiuser/updateAvatar/" + userInfo._id
-                        let obj1 = { avatar: img_base64 }
-                        fetch(url_api, {
-                            method: 'PUT',
-                            headers: {
-                                Accept: 'application/json',
-                                'Content-Type': 'application/json',
-                            },
-                            body: JSON.stringify(obj1),
-                        }).then(async (res) => {
-                            if (res.status === 200) {
-                                         saveImageToStorage(img_base64);
-                            } else {
-                                alert("Có lỗi xảy ra!")
-                                console.log(res);
-                                return res;
-                            }
-                        }).catch((err) => {
-                            console.log(err);
-                        });
+                       
                     });
+            } else {
+                // Image selection canceled, set img_base64 to null
+                setiimg_base64(null);
             }
-
-            // chuyển ảnh thành base64 để upload lên json   
         }
-
     }
+    
+   
+    useEffect(() => {
+        if (img_base64 !== null) {
+            
+            let url_api = "http://" + ip + ":3000/apiuser/updateAvatar/" + userInfo._id;
+            let obj1 = { avatar: img_base64 };
+    
+            fetch(url_api, {
+                method: 'PUT',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(obj1),
+            }).then(async (res) => {
+                if (res.status === 200) {
+                    // Save the image to storage
+                    saveImageToStorage(img_base64, true);
+
+                } else {
+                    alert("Có lỗi xảy ra!")
+                    console.log(res);
+                }
+            }).catch((err) => {
+                console.log(err);
+            });
+        }
+    }, [img_base64]);
+    
     const editUser = async () => {
         const addressRegex = /^[0-9A-Za-z\s,-]+$/;
         if (!addressRegex.test(address)) {
@@ -192,7 +252,11 @@ const EditProfile = (props) => {
             <View style={{ backgroundColor: "#666b7b", width: "auto", height: 100, marginTop: 10, alignItems: "center" }}>
                 <TouchableOpacity onPress={pickImage}>
                     {img_base64 ? (
-                        <Image source={{ uri: img_base64 }} style={{ width: 120, height: 120, borderWidth: 0.5, borderRadius: 100, marginTop: 20 }} />
+                        <ImageBackground style={{ width: 120, height: 120, borderWidth: 0.5, borderRadius: 100, marginTop: 20,borderWidth:1 }} imageStyle={{ borderRadius: 100 }} source={{ uri: img_base64 }}>
+                        <View style={{ flex: 1, justifyContent: "flex-end", alignItems: "flex-end" }}>
+                            <Icons name='camera' size={30} color={'black'} style={{ opacity: 0.7, alignItems: "center", justifyContent: "center", borderWidth: 1, borderColor: "#fff", borderRadius: 10 }} />
+                        </View>
+                    </ImageBackground>
                     ) : (
                         (saveImage && typeof saveImage === 'string') ? (
                             <ImageBackground style={{ width: 120, height: 120, borderWidth: 0.5, borderRadius: 100, marginTop: 20 }} imageStyle={{ borderRadius: 100 }} source={{ uri: saveImage }}>
